041e2ecf14b0909ca5afd724dab905045a0d68c9
Benjamin Auder <benjamin.auder@somewhere>
2017-03-18 02:47:09 +0100

    More readable initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -2,15 +2,20 @@
 
 #initialize submodules, set-up .git/config and .gitattributes, and pre-push hook
 git submodule init && git submodule update --merge
+
 #filter for Jupyter
 python .nbstripout/nbstripout.py --install --attributes .gitattributes
+
 #filter for git-fat [TODO: idempotent...]
 printf '*.pdf filter=fat\n*.tar.xz filter=fat\n*.png filter=fat\n*.jpg filter=fat\n*.ps filter=fat\n'  >> .gitattributes
+
 #pre-push hook: git fat push, submodules update
 printf '#!/bin/sh\n./.git-fat/git-fat pull\n./.git-fat/git-fat push\ngit submodule update --merge\n' > .git/hooks/pre-push
 chmod 755 .git/hooks/pre-push
+
 #.gitfat file with remote on gitfat@auder.net
 printf '[rsync]\nremote = gitfat@auder.net:~/files/valse\n' > .gitfat
+
 #manual git-fat init: with relative path to binary
 #1] remove filter if exists http://stackoverflow.com/questions/12179437/replace-3-lines-with-another-line-sed-syntax
 sed -i '1N;$!N;s/\[filter "fat"\]\n.*\n.*//;P;D' .git/config
