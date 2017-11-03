7fd371e5317f9c61fe5a32daadbbac1c64b2dd31
Benjamin Auder <benjamin.auder@somewhere>
2017-11-03 10:59:17 +0100

    add some TODO and skeleton for test/test_EMGrank.R

---
+++ test/test_EMGrank.R
@@ -0,0 +1,16 @@
+library(valse)
+testFolder = "data/"
+
+# NOTE: R typing is really terrible. as.double as.matrix ...and so on; don't remove.
+
+#get inputs
+#TODO
+
+#get outputs
+#TODO
+
+res = valse::EMGrank(...)
+
+#compare outputs
+nd=7 #number of digits
+#TODO
