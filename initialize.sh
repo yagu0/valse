f09ffee7233a645d840fe8ad2300fdc75ae448a5
Benjamin Auder <benjamin.auder@somewhere>
2021-01-08 16:18:52 +0100

    Simplfy initialize.sh script

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Script for devs
 
 #initialize submodules, set-up .git/config and .gitattributes, and pre-push hook
 git submodule init && git submodule update --merge
@@ -24,11 +25,4 @@ echo 'if (! "formatR" %in% rownames(installed.packages()))
 #.gitfat file with remote on gitfat@auder.net
 printf '[rsync]\nremote = gitfat@auder.net:~/files/valse\n' > .gitfat
 
-#manual git-fat init: with relative path to binary
-#1] remove filter if exists http://stackoverflow.com/a/12179641/4640434
-sed -i '1N;$!N;s/\[filter "fat"\]\n.*\n.*//;P;D' .git/config
-#2] place new filter
-printf \
-'[filter "fat"]
-	clean = ./.git-fat/git-fat filter-clean
-	smudge = ./.git-fat/git-fat filter-smudge\n' >> .git/config
+#Now run git-fat init :)
